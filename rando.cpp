9a1c0d3bbe1998138d5e52a7ffdcf1ef435343c2
Komalgumma <ongkomal@gmail.com>
2025-09-06 16:33:46 +0530

    Palindrom question basic algo

---
+++ rando.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string y = to_string(x);
+        for(int i = 0; i < y.size(); i++){
+            if(y[i] != y[y.size()-1-i]){return false;}
+        }
+        return true;
+    }
+};
