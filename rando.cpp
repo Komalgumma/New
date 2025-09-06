976490aedcb6d9d46ec59121d8eaab4b113e305c
Komalgumma <ongkomal@gmail.com>
2025-09-06 16:51:07 +0530

    removed a redundancy

--- rando.cpp
+++ rando.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string y = to_string(x);
-        for(int i = 0; i < y.size(); i++){
-            if(y[i] != y[y.size()-1-i]){return false;}
+        if(x<0){return false;}
+        int y = abs(x);
+        vector<int> z;
+        while(y>0){
+            z.push_back(y%10);
+            y /= 10;
+        }
+        for(int i = 0; i<z.size(); i++){
+            if(z[i] != z[z.size()-1-i]){return false;}
         }
         return true;
     }
